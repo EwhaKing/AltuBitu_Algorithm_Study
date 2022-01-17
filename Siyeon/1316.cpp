d88211088bc353aa1967a3b05576ec99405c4400
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2022-01-18 00:02:55 +0900

    Update 1316.cpp

--- Siyeon/1316.cpp
+++ Siyeon/1316.cpp
@@ -4,35 +4,27 @@
 using namespace std;
 
 bool compare(string str) {
-    char cur = '.'; //현재의 문자
-    vector<bool> alphabet(26, false); //각 알파벳의 등장 여부를 체크
+    char cur = '.'; 
+    vector<bool> alphabet(26, false); 
 
     for (int i = 0; i < str.size(); i++) {
-        if (str[i] == cur) //연속해서 나타난 문자
-            continue;
-        //이전의 문자와 다른 문자가 나타났을 때
-        if (alphabet[str[i] - 'a']) //이전에 등장했던 문자면 떨어져서 나타난 것
-            return false;
-        cur = str[i]; //현재의 문자 갱신
-        alphabet[str[i] - 'a'] = true; //등장 체크
+        if (str[i] == cur) continue;
+        if (alphabet[str[i] - 'a']) return false;
+        cur = str[i]; 
+        alphabet[str[i] - 'a'] = true;
     }
     return true;
 }
 
 int main() {
     int n, result = 0;
-
-    //입력
     cin >> n;
     while (n--) {
         string word;
         cin >> word;
-
-        //연산
-        if (compare(word)) //그룹 단어인지 확인
+        if (compare(word)) 
             result++;
     }
-
-    //출력
+    
     cout << result;
 }
