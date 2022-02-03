7a8b5951608b0864509af4157ba3a3281b287bc0
Yongmalyang <37902200+Yongmalyang@users.noreply.github.com>
2022-02-03 22:35:54 +0900

    스터디 6회차

---
+++ Juyeon/2609.cpp
@@ -0,0 +1,36 @@
+/*
+220203
+¿Ã¡÷ø¨
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int gcdBad(int a, int b) {
+    for (int i = min(a, b); i > 1; i--) {
+        if (a % i == 0 && b % i == 0) {
+            return i;
+        }
+    }
+    return 1; 
+}
+
+int gcdRecursion(int a, int b) {
+    if (b == 0) 
+        return a;
+    return gcdRecursion(b, a % b);
+}
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    int g = gcdRecursion(max(a, b), min(a, b));
+    int l = (a * b) / g;
+
+    cout << g << '\n' << l << '\n';
+
+    return 0;
+}
