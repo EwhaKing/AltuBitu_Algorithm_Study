721cef08382ee3b90297c0358029fe9b8563bdfa
Sunny Lee <80109963+sunnyineverywhere@users.noreply.github.com>
2022-02-17 23:15:58 +0900

    동전 0

---
+++ SEON_UI_LEE/9. Greedy/BOJ11047.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+
+	int n, k;
+	cin >> n >> k;
+	vector<int> coins(n);
+	for (int i = 0; i < n; i++) {
+		cin >> coins[i];
+	}
+
+	int cnt = 0;
+
+	for (int i = n - 1; i >= 0; i--) {
+		if (k == 0) {
+			break;
+		}
+		if (coins[i] <= k) {
+			cnt += (k / coins[i]);
+			k %= coins[i];
+		}
+	}
+
+	cout << cnt;
+
+	return 0;
+}
