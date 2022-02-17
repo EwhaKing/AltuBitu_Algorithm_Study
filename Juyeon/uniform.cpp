4e25595ca7596c77b7f453c16e1784ea7ac89cb0
Yongmalyang <37902200+Yongmalyang@users.noreply.github.com>
2022-02-17 23:17:40 +0900

    스터디 9회차

---
+++ Juyeon/uniform.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    vector<int> student(n + 1, 1); 
+
+    for (int i = 0; i < lost.size(); i++) 
+        student[lost[i]]--;
+
+    for (int i = 0; i < reserve.size(); i++) 
+        student[reserve[i]]++;
+
+    for (int i = 1; i <= n; i++) { 
+        if (student[i]) 
+            continue;
+        if (student[i - 1] == 2) { 
+            student[i - 1]--;
+            student[i]++;
+        }
+        else if (student[i + 1] == 2) { 
+            student[i + 1]--;
+            student[i]++;
+        }
+
+    }
+
+    int answer = 0;
+    for (int i = 1; i <= n; i++) {
+        if (student[i]) 
+            answer++;
+    }
+
+    return answer;
+}
+
+int main() {
+    vector<int> lost = { 2, 4 };
+    vector<int> reserve = { 1, 3, 5 };
+    int n = 5;
+
+    cout << solution(n, lost, reserve);
+
+    return 0;
+}
